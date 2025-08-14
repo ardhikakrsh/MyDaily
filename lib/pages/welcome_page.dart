56810ad1db50188ebd9b71a97658418046d0fc03
ardhikakrsh <ardhikakrishna@gmail.com>
2025-08-14 11:02:53 +0700

    feat: add dark theme for welcome page

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:mydaily/widgets/main_background.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mydaily/widgets/dark_secondary_background.dart';
 import 'package:mydaily/widgets/navigation_menu.dart';
+import 'package:mydaily/widgets/secondary_background.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final isDarkMode = Theme.of(context).brightness == Brightness.dark;
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
         children: [
-          MainBackground(),
+          isDarkMode
+              ? const DarkSecondaryBackground()
+              : const SecondaryBackground(),
           Center(
             child: Image.asset(
               'assets/images/logo_mydaily.png',
@@ -20,14 +24,35 @@ class WelcomePage extends StatelessWidget {
               height: 300,
             ),
           ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const NavigationMenu()),
-              );
-            },
-            child: const Text('Get Started'),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 60.0, left: 30, right: 30),
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const NavigationMenu(),
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(horizontal: 60, vertical: 12),
+                  ),
+                  child: Text(
+                    'Get Started',
+                    style: GoogleFonts.rubik(
+                      fontSize: 16,
+                      letterSpacing: 2,
+                      color: isDarkMode ? Color(0xFFB2A5FF) : Color(0xFF8B4CFC),
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ),
         ],
       ),
